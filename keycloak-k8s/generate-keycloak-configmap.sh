034c8c38b6bf7c42957159645381124e8b3dace2
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-05-02 00:15:40 +0200

    Fix keycloak

--- keycloak-k8s/generate-keycloak-configmap.sh
+++ keycloak-k8s/generate-keycloak-configmap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl create configmap keycloak-config --from-file ../../docker-compose/realm-config/ --dry-run=client --output=yaml > keycloak-config.yml
+kubectl create configmap keycloak-config --from-file ../../docker-compose/realm-config/ --dry-run=client --output=yaml --namespace fridgy > keycloak-config.yml
  
